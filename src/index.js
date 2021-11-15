ccf860f2231290d531f11bbe49d4596d6406c550
aziz <allassaniabdoulaziz@gmail.com>
2021-11-15 12:00:24 +0000

    maj

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
-import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
